f251607d870387a798e09f98bfa297892eee4c2f
99hyuk <yh99cho1@gmail.com>
2025-06-30 17:44:22 +0900

    [FEAT] 유저 조회, 비활성화 API 구현 / WebMvcConfig 파일 통해 @userId 어노테이션 활성화 (resolver 등록)

---
+++ src/main/java/com/sumte/user/controller/UserController.java
@@ -0,0 +1,45 @@
+package com.sumte.user.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sumte.apiPayload.ApiResponse;
+import com.sumte.security.authorization.UserId;
+import com.sumte.user.dto.response.UserInfoResponse;
+import com.sumte.user.service.UserService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Tag(name = "사용자 관련 API", description = "사용자 관련 API 입니다.")
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+	private final UserService userService;
+
+	//유저 조회
+	@GetMapping
+	@Operation(summary = "유저 정보 조회 API", description = "특정 유저의 정보를 조회합니다.")
+	public ResponseEntity<UserInfoResponse> getUserInfo(
+		@UserId Long userId) {
+		UserInfoResponse response = userService.getUserInfo(userId);
+		return ResponseEntity.ok(response);
+	}
+
+	//유저 탈퇴(비활성화)
+	@Operation(summary = "유저 비활성화 API", description = "유저 탈퇴 시 비활성화 처리를 합니다.")
+	@DeleteMapping
+	public ResponseEntity<ApiResponse<Void>> deactivateUser(
+		@UserId Long userId) {
+		userService.deactivateUser(userId);
+		return ResponseEntity.ok(ApiResponse.successWithNoData());
+	}
+}
