a4d973ad5043b9baeae01f013d56c45e5fd0ef4c
hyowon <ally0204@naver.com>
2025-07-03 10:15:47 +0900

    [mod/#4] @UserId로 수정

--- src/main/java/com/sumte/reservation/controller/ReservationController.java
+++ src/main/java/com/sumte/reservation/controller/ReservationController.java
@@ -3,6 +3,7 @@ package com.sumte.reservation.controller;
 import com.sumte.apiPayload.ApiResponse;
 import com.sumte.apiPayload.exception.annotation.CheckPage;
 import com.sumte.apiPayload.exception.annotation.CheckPageSize;
+import com.sumte.security.authorization.UserId;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
@@ -21,8 +22,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 
-import java.util.List;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/reservations")
@@ -35,7 +34,7 @@ public class ReservationController {
 	@PostMapping
 	@Operation(summary = "예약 생성 API", description = "요청 본문으로 객실 ID, 투숙 인원, 날짜 정보를 입력받고, 헤더의 userId를 통해 예약을 생성합니다.")
 	public ResponseEntity<ApiResponse<Void>> createReservation(
-			@Parameter(description = "요청한 사용자 ID") @RequestHeader("userId") Long userId,
+			@Parameter(description = "요청한 사용자 ID") @UserId Long userId,
 			@Valid @RequestBody ReservationRequestDTO.CreateReservationDTO request
 	) {
 		reservationService.createReservation(request, userId);
@@ -45,7 +44,7 @@ public class ReservationController {
 	@GetMapping("/my")
 	@Operation(summary = "내 예약 목록 조회 API", description = "내가 예약한 숙소 목록을 페이지 단위로 조회합니다.")
 	public ResponseEntity<ApiResponse<Page<ReservationResponseDTO.MyReservationDTO>>> getMyReservations(
-			@Parameter(description = "요청한 사용자 ID") @RequestHeader("userId") Long userId,
+			@Parameter(description = "요청한 사용자 ID") @UserId Long userId,
 			@CheckPage @RequestParam(name = "page", defaultValue = "1") int page,
 			@CheckPageSize @RequestParam(name = "size", defaultValue = "10") int size
 	) {
@@ -57,7 +56,7 @@ public class ReservationController {
 	@GetMapping("/{id}")
 	@Operation(summary = "예약 상세 조회 API", description = "예약 ID를 기준으로 상세 정보를 조회합니다.")
 	public ResponseEntity<ApiResponse<ReservationResponseDTO.ReservationDetailDTO>> getReservationDetail(
-			@Parameter(description = "요청한 사용자 ID") @RequestHeader("userId") Long userId,
+			@Parameter(description = "요청한 사용자 ID") @UserId Long userId,
 			@PathVariable("id") Long reservationId
 	) {
 		ReservationResponseDTO.ReservationDetailDTO result = reservationService.getReservationDetail(reservationId, userId);
@@ -67,7 +66,7 @@ public class ReservationController {
 	@PatchMapping("/{id}")
 	@Operation(summary = "예약 취소 API", description = "예약 ID를 기준으로 사용자의 예약을 취소합니다.")
 	public ResponseEntity<ApiResponse<Void>> cancelReservation(
-			@Parameter(description = "요청한 사용자 ID") @RequestHeader("userId") Long userId,
+			@Parameter(description = "요청한 사용자 ID") @UserId Long userId,
 			@PathVariable("id") Long reservationId
 	) {
 		reservationService.cancelReservation(reservationId, userId);
