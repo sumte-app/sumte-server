cd7b184b185877f8ed8c30c6ef230d6c920c9dd9
eeeeeaaan <wjdwjddla73@naver.com>
2025-07-05 09:57:55 +0900

    [FEAT] 부가서비스 전체 조회

--- src/main/java/com/sumte/guesthouse/dto/OptionResponseDTO.java
+++ src/main/java/com/sumte/guesthouse/dto/OptionResponseDTO.java
@@ -27,4 +27,22 @@ public class OptionResponseDTO {
 		List<Target> targets;
 	}
 
+	@Builder
+	@Getter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class Option {
+		String name;
+	}
+
+	// 페이징 처리 안한 이유는 어차피 targetAudience 나 OptionService같은 경우엔
+	// 굳이 필요 없을 것 같기 때문입니다. 전체 조회 api 이기도 하구요
+	@Builder
+	@Getter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class OptionList {
+		List<Option> options;
+	}
+
 }
--- src/main/java/com/sumte/guesthouse/service/OptionQueryService.java
+++ src/main/java/com/sumte/guesthouse/service/OptionQueryService.java
@@ -1,5 +1,29 @@
 package com.sumte.guesthouse.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.sumte.guesthouse.converter.OptionServiceConverter;
+import com.sumte.guesthouse.dto.OptionResponseDTO;
+import com.sumte.guesthouse.entity.OptionServices;
+import com.sumte.guesthouse.repository.OptionServicesRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class OptionQueryService {
+
+	private final OptionServicesRepository optionServicesRepository;
+	private final OptionServiceConverter optionServiceConverter;
+
+	public OptionResponseDTO.OptionList getAllOptionServices() {
+		List<OptionServices> optionServicesList = optionServicesRepository.findAll();
+
+		OptionResponseDTO.OptionList optionList = optionServiceConverter.toOptionList(optionServicesList);
+
+		return optionList;
+	}
 
 }
